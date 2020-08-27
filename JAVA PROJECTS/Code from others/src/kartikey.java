61a8b4c867e18b319fbf05cae810585970937ad7
Tanmay <Tanmaysinghal380@gmail.com>
2020-08-27 14:14:22 +0530

    python change

--- JAVA PROJECTS/Code from others/src/kartikey.java
+++ JAVA PROJECTS/Code from others/src/kartikey.java
@@ -8,7 +8,6 @@ class kartikey {
     static boolean again;
 
     static int score;
-
     static void swap(int row1, int col1, int row2, int col2) {
         int temp = matrix[row1][col1];
         matrix[row1][col1] = matrix[row2][col2];
---
+++ Python/PycharmProjects/WEB/openclass.py
@@ -0,0 +1,27 @@
+import time
+
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.alert import Alert
+
+r=requests.get("https://learn.upes.ac.in/")
+driver=webdriver.Chrome("D:\Tanmay Singhal\Downloads\chromedriver.exe")
+driver.get("https://learn.upes.ac.in/")
+driver.find_element_by_id("user_id").send_keys("500069450@stu.upes.ac.in")
+driver.find_element_by_id("password").send_keys("tanmay427462")
+driver.find_element_by_id("entry-login").click()
+time.sleep(2)
+driver.find_element_by_class_name("button-1").click()
+time.sleep(1)
+driver.find_element_by_link_text("Advanced Game Programming Algorithms.BT-CSE-SPZ-GG-V-B1.BT-CSE-SPZ-GG-V-B2.VR_B_312").click()
+time.sleep(1)
+driver.find_element_by_id("menuPuller").click()
+time.sleep(1)
+driver.find_element_by_partial_link_text("Collaborate").click()
+time.sleep(9)
+
+
+# driver.quit()
+# driver.find_element_by_id("menuPuller").click()
+# driver.find_element_by_class_name("courseMenu")
+# driver.find_element_by_xpath('//*[@title="Discussions"]').click()
